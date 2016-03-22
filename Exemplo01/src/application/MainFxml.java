e3c16f4028d8d36d45c7f3440ccdbca6ee93fd28
Bruno Colaco <Bruno@>
2016-03-22 20:29:53 -0300

    Update 22/03/2016

---
+++ Exemplo01/src/application/MainFxml.java
@@ -0,0 +1,28 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class MainFxml extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Pane root = (Pane) FXMLLoader.load(getClass().getResource("HelloWorld.fxml"));
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ Exemplo01/src/controler/HelloWorldController.java
@@ -0,0 +1,29 @@
+package controler;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class HelloWorldController {
+
+	@FXML
+	private TextField tfNome;
+
+	@FXML
+	private Button btnSalvar;
+
+	@FXML
+	private Button btnExcluir;
+
+	@FXML
+	void excluir(ActionEvent event) {
+
+	}
+
+	@FXML
+	void salvar(ActionEvent event) {
+
+	}
+
+}
